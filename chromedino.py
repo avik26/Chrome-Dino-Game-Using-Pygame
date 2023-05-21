cde8f803e04327577dbd1008ced483319cc6e5e3
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-05-21 23:16:23 +0530

    Update chromedino.py

--- chromedino.py
+++ chromedino.py
@@ -11,7 +11,6 @@ pygame.init()
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 pygame.display.set_caption("Chrome Dino Runner")
 
 Ico = pygame.image.load("assets/DinoWallpaper.png")
